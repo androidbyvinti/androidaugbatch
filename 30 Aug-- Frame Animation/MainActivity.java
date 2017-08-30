f54598101a6d78276a35e840a888b1ec9b543c4c
vintisachdeva <vintisachdeva17@gmail.com>
2017-08-30 20:12:53 +0530

    Add files via upload

---
+++ 30 Aug-- Frame Animation/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bmpl.frameanimation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.frameanimation);
+
+        animationDrawable =(AnimationDrawable)imageView.getBackground();
+        animationDrawable.start();
+    }
+}
